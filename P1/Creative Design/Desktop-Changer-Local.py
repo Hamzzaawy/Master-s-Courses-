eeec5784428f94ef7e807409e0de7b091b5803ef
Hamza Abdalla <49972606+Hamzzaawy@users.noreply.github.com>
2020-10-21 13:07:23 +0300

    Update Desktop-Changer-Local.py

--- P1/Creative Design/Desktop-Changer-Local.py
+++ P1/Creative Design/Desktop-Changer-Local.py
@@ -2,7 +2,8 @@ import ctypes
 import os
 import time
 
-directory = "E:\\images\\Backgrounds"
+#add the directory to the folder containing the images you want to sweep between as desktop wallpaper
+directory = "E:\\images\\Backgrounds" 
 def changeBG():
     while True:
         for root,dirs,filenames in os.walk(directory):
@@ -11,8 +12,10 @@ def changeBG():
                     start=time.time()
                     SPI_SETDESKTOPWALLPAPER=20
                     image = directory + "\\" + i
+                    #print the directory to the image to be displayed 
                     print(image)
                     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKTOPWALLPAPER,0,image,3)
+                    #time interval between each desktop wallpaper change
                     time.sleep(10)
                     
 changeBG()

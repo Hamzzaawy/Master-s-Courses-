7631024ea58288539b97dee2f838939b16f5ad41
Hamza Abdalla <49972606+Hamzzaawy@users.noreply.github.com>
2020-12-19 16:48:16 +0200

    Update sheets.py

--- IoT/Gsheets/sheets.py
+++ IoT/Gsheets/sheets.py
@@ -15,60 +15,6 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SAMPLE_SPREADSHEET_ID = '1Ne6y6YpC4ypZSpLgi0EI_QsgSIuR-p-xbojtYIP5XkQ'
 SAMPLE_RANGE_NAME = "B3:B13"
 KEY = 'AIzaSyBDq3h_jepqUIYJj799-PBoHxb_vauiNYI' 
-
-# def main():
-#     """Shows basic usage of the Sheets API.
-#     Prints values from a sample spreadsheet.
-#     """
-#     creds = None
-#     # The file token.pickle stores the user's access and refresh tokens, and is
-#     # created automatically when the authorization flow completes for the first
-#     # time.
-#     if os.path.exists('tokenSheets.pickle'):
-#         with open('tokenSheets.pickle', 'rb') as token:
-#             creds = pickle.load(token)
-#     # If there are no (valid) credentials available, let the user log in.
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 'credentialsSheets.json', SCOPES)
-#             creds = flow.run_local_server(port=0)
-#         # Save the credentials for the next run
-#         with open('tokenSheets.pickle', 'wb') as token:
-#             pickle.dump(creds, token)
-
-
-#     # discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-#     #                 'version=v4')
-#     # service = discovery.build(
-#     #     'sheets',
-#     #     'v4',
-#     #     http=httplib2.Http(),
-#     #     discoveryServiceUrl=discoveryUrl,
-#     #     developerKey=KEY)
-
-#     service = build('sheets', 'v4', credentials=creds)
-
-#     # Call the Sheets API
-#     sheet = service.spreadsheets()
-#     result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                                 range=SAMPLE_RANGE_NAME).execute()
-#     values = result.get('values', [])
-#     print(values)
-
-#     # if not values:
-#     #     print('No data found.')
-#     # else:
-#     #     print('Name, Major:')
-#     #     for row in values:
-#     #         # Print columns A and E, which correspond to indices 0 and 4.
-#     #         print('%s' % (row[0]))
-
-# # if __name__ == '__main__':
-#     main()
-
 def InitSheets():
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
